ea09eae54faff4c00629c7f5b8a36effbebeb211
unknown <tanbiirsakiib7@gmail.com>
2021-09-30 23:02:34 +0600

    ranga title

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ const loadProducts = () => {
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
+  console.log(allProducts);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
+
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
